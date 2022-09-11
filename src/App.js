95e3492913eef862fe0126b88135075ff9c63c91
Harvey Javier <harveyjavier17@gmail.com>
2022-09-11 14:56:11 +0800

    Add own Infura key, limit user access by providing passcode, and limit file size

--- src/App.js
+++ src/App.js
@@ -2,32 +2,53 @@ import React from 'react';
 import './App.css';
 import { create } from 'ipfs-http-client';
 import logo from './images/logo.png';
-const client = create('https://ipfs.infura.io:5001/api/v0');
+import { Buffer } from "buffer";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fileURL: "",
+      passcode: "",
       error: "",
     };
   }
 
   async onChange(e) {
-    const file = e.target.files[0]
-    try {
-      const added = await client.add(file)
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      this.setState({ fileURL:url, error:"" });
-      console.log(url);
-    } catch (error) {
-      this.setState({ error:error, fileURL:"" });
-      console.log('Error uploading file: ', error);
-    }  
+    const file = e.target.files[0];
+    if (this.state.passcode !== process.env.REACT_APP_Password) {
+      this.setState({ error: "Wrong passcode.", fileURL:"" });
+    } else if (file.size > 10e5) {
+      this.setState({ error: "Please upload a file smaller than 1 MB", fileURL:"" });
+    } else {
+      try {
+        const auth = "Basic " + Buffer.from(`${process.env.REACT_APP_ProjectId}:${process.env.REACT_APP_ProjectSecret}`).toString("base64");
+        const client = create({
+          host: "ipfs.infura.io",
+          port: 5001,
+          protocol: "https",
+          headers: {
+            authorization: auth,
+          },
+        });
+        const added = await client.add(file);
+        //console.log(added);
+        const url = `https://ipfs.io/ipfs/${added.path}`
+        this.setState({ fileURL:url, error:"" });
+        //console.log(url);
+      } catch (error) {
+        this.setState({ error:error, fileURL:"" });
+        //console.log('Error uploading file: ', error);
+      }
+    }
   }
 
   uploadAgain() {
     this.setState({ fileURL:"", error:"" });
+  }
+
+  handleInput(state, event) {
+    this.setState({ [state]: event.target.value });
   }
 
   render() {
@@ -48,11 +69,20 @@ class App extends React.Component {
                       <div className={ ((this.state.fileURL === "" && this.state.error === "") && "form-group" ) + " col-12" }>
                         { (this.state.fileURL === "" && this.state.error === "") &&
                           <div>
-                            <input type="file" className="form-control form-control-lg" accept="image/png, image/gif, image/jpeg" onChange={(e) => this.onChange(e)} />
+                            <div className="row">
+                              <div className="col-6">
+                                <input type="name" className="form-control form-control-lg" value={this.state.passcode} onChange={(e) => this.handleInput("passcode", e)} placeholder="Passcode" />
+                              </div>
+                              <div className="col-6">
+                                <input type="file" className="form-control form-control-lg" accept="image/png, image/gif, image/jpeg" onChange={(e) => this.onChange(e)} />
+                              </div>
+                            </div>
                             <br/>
                             <div className="alert alert-info">
-                              {/*<strong>File uploaded successfully!</strong><br/>*/}
                               Upload an image and it will be automatically uploaded to IPFS.
+                            </div>
+                            <div className="alert alert-warning">
+                              Due to limited amount of storage, we only allow 1 MB file size and limit the access of this uploader to <a href="https://lrn.ac/bdb" target="_blank" rel="noreferrer">BDB 2022</a> students. As for the passcode, mentor <code>harvz</code> will provide it on <code>🔒bdb2022</code> Slack channel.
                             </div>
                           </div>
                         }
@@ -62,7 +92,7 @@ class App extends React.Component {
                               <strong>File uploaded successfully!</strong><br/>
                               Here's your uploaded image: <a href={this.state.fileURL} target="_blank" rel="noreferrer">{ this.state.fileURL }</a>
                             </div>
-                            <img src={this.state.fileURL} className="img-thumbnail" alt={this.state.fileURL.replace("https://ipfs.infura.io/ipfs/", "")} />
+                            <img src={this.state.fileURL} className="img-thumbnail" alt={this.state.fileURL.replace("https://ipfs.io/ipfs/", "")} />
                             <br/><br/>
                           </div>
                         }
